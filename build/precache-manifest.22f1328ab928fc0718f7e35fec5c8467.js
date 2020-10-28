21024a89cd8a60875e81c942a730d5369539074d
Mary-the-cherry <meyer.maren100@googlemail.com>
2020-10-28 11:49:20 -0300

    with build folder

---
+++ build/precache-manifest.22f1328ab928fc0718f7e35fec5c8467.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e49c17298545308efabb74331d08a99",
+    "url": "/index.html"
+  },
+  {
+    "revision": "be8e8e0df990c3d1d6c2",
+    "url": "/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "6afc4a64c6d26f699ae0",
+    "url": "/static/css/main.b6b04697.chunk.css"
+  },
+  {
+    "revision": "be8e8e0df990c3d1d6c2",
+    "url": "/static/js/2.562bab00.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.562bab00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6afc4a64c6d26f699ae0",
+    "url": "/static/js/main.a54d465b.chunk.js"
+  },
+  {
+    "revision": "fbaab9a582e8e68a6c8c",
+    "url": "/static/js/runtime-main.2ae115fb.js"
+  },
+  {
+    "revision": "1c72449cc7b347e3c7a9ed1fed771f75",
+    "url": "/static/media/birthday.1c72449c.jpg"
+  }
+]);
